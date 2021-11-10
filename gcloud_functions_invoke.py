77bf599c3e90535133e7012983053a06e996cbf8
jonathanandrade10 <jonathanandrade10@gmail.com>
2021-11-10 15:13:25 +0000

    Create gcloud_functions_invoke.py

---
+++ gcloud_functions_invoke.py
@@ -0,0 +1,20 @@
+import os
+
+import google.auth.transport.requests
+import google.oauth2.id_token
+
+#Environment variable setup that holds the principal json credential
+
+key_path = "json-auth-path/account-id.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = key_path
+
+service_url = "https://gcp-region-projectid.cloudfunctions.net/my_function_to_run"
+req = urllib.request.Request(service_url)
+
+auth_req = google.auth.transport.requests.Request()
+id_token = google.oauth2.id_token.fetch_id_token(auth_req, service_url)
+
+req.add_header("Authorization", f"Bearer {id_token}")
+response = urllib.request.urlopen(req)
+
+print(response.read())
